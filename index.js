da4382de22e9823027e1c3dad462fcf051e67a9a
Andur909 <147121202+Andur909@users.noreply.github.com>
2023-12-17 20:17:55 -0500

    Added new project/quiz and linked it

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ window.addEventListener("load",addListeners)
 
 function addListeners()
 {
-	document.getElementById("btncalculator").addEventListener("click",calculation)
+	document.getElementById("btncalculator").addEventListener("click",calculation);
+	document.getElementById("btnquizloops").addEventListener("click",quizloops);
 }
 
 function calculation()
 {
 	window.location.href = "CalculatorIndex.html"
+}
+
+function quizloops()
+{
+	window.location.href = "Quiz.html";
 }
